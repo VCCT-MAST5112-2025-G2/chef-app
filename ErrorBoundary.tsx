46f3cfa8e9c27a5b7b4d332e311468285b3ed502
callum <st10474335@vcconnect.edu.za>
2025-11-12 10:53:03 +0200

    added error handling

---
+++ ErrorBoundary.tsx
@@ -0,0 +1,61 @@
+import React, { Component, ErrorInfo, ReactNode } from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+interface Props {
+  children: ReactNode;
+}
+
+interface State {
+  hasError: boolean;
+}
+
+class ErrorBoundary extends Component<Props, State> {
+  public state: State = {
+    hasError: false
+  };
+
+  public static getDerivedStateFromError(_: Error): State {
+    // Update state so the next render will show the fallback UI.
+    return { hasError: true };
+  }
+
+  public componentDidCatch(error: Error, errorInfo: ErrorInfo) {
+    // You can also log the error to an error reporting service
+    console.error("Uncaught error:", error, errorInfo);
+  }
+
+  public render() {
+    if (this.state.hasError) {
+      // You can render any custom fallback UI
+      return (
+        <View style={styles.container}>
+          <Text style={styles.title}>Oops, something went wrong.</Text>
+          <Text style={styles.subtitle}>We are working on fixing it.</Text>
+        </View>
+      );
+    }
+
+    return this.props.children;
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#8b63c3",
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "white",
+    marginBottom: 10,
+  },
+  subtitle: {
+    fontSize: 16,
+    color: "white",
+  },
+});
+
+export default ErrorBoundary;
